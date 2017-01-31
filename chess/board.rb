8296aa666cf72d05c81ed095b356ea5d13238544
App Academy Student <student@appacademy.io>
2017-01-30 17:04:20 -0800

    populate board

--- chess/board.rb
+++ chess/board.rb
@@ -3,22 +3,37 @@ require_relative "piece.rb"
 class Board
     attr_reader :grid
     def initialize()
-      @grid = Array.new(8) { Array.new(8) {nil} }
+      @grid = Array.new(8) { Array.new(8) }
+      create_board
+
     end
 
     def move_piece(start_pos, end_pos)
-
-      raise "Board Parameter Error" unless valid_move?(end_pos)
+      raise "No piece in start position" unless valid_move?(start_pos)
+      raise "End position not valid" unless valid_move?(end_pos)
 
       current_piece = self[start_pos]
       other_piece = self[end_pos]
 
-      unless valid_move?(end_pos)
-        raise "Board Parameter Error"
-      end
-
       self[start_pos] = other_piece
       self[end_pos] = current_piece
+    end
+
+    def create_board
+      row = 0
+      while row < @grid.length
+        col = 0
+        while col < @grid.length
+          if [0,1,6,7].include?(row)
+            self[[row,col]] = "a" #Piece.new()
+          else
+            self[[row,col]] = nil
+          end
+          col += 1
+        end
+        row += 1
+      end
+
     end
 
     def valid_move?(pos)
