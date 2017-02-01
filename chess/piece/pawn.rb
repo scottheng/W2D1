99a9a1eb461be4b4f3ca4760ac5fec1c81bad935
App Academy Student <student@appacademy.io>
2017-01-31 17:54:11 -0800

    Create Pawn

--- chess/piece/pawn.rb
+++ chess/piece/pawn.rb
@@ -1,16 +1,54 @@
 require_relative "../piece.rb"
 
-class Pawn
+class Pawn < Piece
+
+  def initialize()
+    super(name, position, board)
+    @moved = false
+  end
+
+  DIAGONAL = [[1,-1], [1,1]]
+  UP = [0,1]
+  OPTION_UP = [0,2]
+
 
   def moves
+    possible_moves = []
 
+    DIAGONAL.each do |delta|
+      current_position = new_position(self.position, delta)
+
+      if in_bounds?(current_position) && check_diagonal?(current_position)
+        possible_moves << current_position
+      end
+    end
+
+    possible_up = [UP]
+    possible_up.concat(OPTION_UP) unless @moved
+
+    possible_up.each do |delta|
+      current_position = new_position(self.position, delta)
+
+      if in_bounds?(current_position) && is_valid?(current_position)
+        possible_moves << current_position
+      end
+    end
+    possible_moves
   end
 
-
-  def is_valid?(pos)
-
+  def new_position(current_position, delta)
+    first = current_position.first + delta.first
+    last = current_position.last + delta.last
+    current_position = [first, last]
   end
 
+  def check_diagonal(pos)
+     !is_valid?(pos)
+   end
+
+  def is_valid?(pos) # Need to find out if the piece is ours?
+    @board[pos].name == "n" ? true : false
+  end
 
   def in_bounds?(pos)
     x, y = pos
